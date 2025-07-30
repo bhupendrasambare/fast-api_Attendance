c41178cf3d0c0df1a1a66e41d592e6ae8a0a8a71
bhupendrasambare <bhupendrasam1404@gmail.com>
2025-07-31 00:28:20 +0530

    Image package added

--- app/students/routes.py
+++ app/students/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends, UploadFile, File
+from fastapi import APIRouter, HTTPException, Depends, UploadFile, File, Form
 from app.auth.auth import get_current_user
 from app.database import students_collection
 from app.students.modules import StudentCreate, StudentUpdate
@@ -13,25 +13,35 @@ router = APIRouter(prefix="/students", tags=["2. Students"])
 BASE_UPLOAD_DIR = os.getenv("LOCAL_LOCATION")
 
 @router.post("/add")
-async def create_student(student:StudentCreate, image:UploadFile = File(...), user: dict = Depends(get_current_user)):
-    filename = f"{student.firstname}_{student.lastname}.{image.filename.split('.')[-1]}"
-    dir_path = Path(BASE_UPLOAD_DIR) / student.session / f"{student.firstname}_{student.lastname}"
+async def create_student(firstname: str = Form(...), middlename: str = Form(...), lastname: str = Form(...), student_class: str = Form(...),
+                        section: str = Form(...), session: str = Form(...), image: UploadFile = File(...), user: dict = Depends(get_current_user)
+):
+    filename = f"{firstname}_{lastname}.{image.filename.split('.')[-1]}"
+    dir_path = Path(BASE_UPLOAD_DIR) / session / f"{firstname}_{lastname}"
     dir_path.mkdir(parents=True, exist_ok=True)
     file_path = dir_path / filename
 
     with open(file_path, "wb") as f:
         shutil.copyfileobj(image.file, f)
 
-    student_dist = student.dict()
-    student_dist["image_name"] = str(file_path)
-    student_dist["created_by"] = str(user["_id"])
-    result = await students_collection.insert_one(student_dist)
-    return {"id":result.inserted_id, "message":"Student created"}
+    student_dict = {
+        "firstname": firstname,
+        "middlename": middlename,
+        "lastname": lastname,
+        "student_class": student_class,
+        "section": section,
+        "session": session,
+        "image_name": str(file_path),
+        "created_by": str(user)
+    }
+
+    result = await students_collection.insert_one(student_dict)
+    return {"id":str(result.inserted_id), "message":"Student created"}
 
 @router.get("/get")
 async def get_students(user: dict = Depends(get_current_user)):
     students = await students_collection.find().to_list()
-    return students
+    return [transform_student(s) for s in students]
 
 @router.put("/update/{student_id}")
 async def update_student(student_id:str, student:StudentUpdate, user :dict = Depends(get_current_user)):
@@ -48,3 +58,6 @@ async def delete_student(student_id:str, user: dict= Depends(get_current_user)):
         raise HTTPException(status_code=404, detail="Student not found")
     return {"message":"Student deleted"}
 
+def transform_student(student):
+    student["_id"] = str(student["_id"])
+    return student
