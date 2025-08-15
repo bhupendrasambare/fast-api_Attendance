275e335e71b033b009647f5dbcbd277d2afbf373
bhupendrasambare <bhupendrasam1404@gmail.com>
2025-08-15 21:19:03 +0530

    Setting page modules

---
+++ app/class/modules.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class createSession(BaseModel):
+    start_year:str
+    end_year:str
+    active:bool
+
+class updateSession(BaseModel):
+    start_year:str
+    end_year:str
+    active:bool
+
--- web/src/pages/Settings.jsx
+++ web/src/pages/Settings.jsx
@@ -4,7 +4,10 @@ import NavigationBar from '../components/Navbar'
 const Settings = () => {
   return (
     <NavigationBar>
-        Settings
+        <div className="shadow-lg py-3 px-1 mx-1 rounded">
+            <h5 className="fw-bold m-2">Sessions</h5>
+
+        </div>
     </NavigationBar>
   )
 }
