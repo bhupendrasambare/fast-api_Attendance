b9412564eb46cb9e0be6e7a6759cf9556b891f1e
bhupendrasambare <bhupendrasam1404@gmail.com>
2025-08-28 22:30:35 +0530

    sessions UI

--- web/src/pages/Settings.jsx
+++ web/src/pages/Settings.jsx
@@ -16,10 +16,25 @@ const Settings = () => {
 
         <div className="w-100 ps-2">
             
+            <div className="d-flex justify-content-between my-3">
+                <div className="session-section">
+                    <button className="btn btn-success shadow-lg rounded-pill mx-2"> 2019 - 2023 </button>
+                    <button className="btn btn-light shadow-lg rounded-pill mx-2"> 2019 - 2023 </button>
+                    <button className="btn btn-light shadow-lg rounded-pill mx-2"> 2019 - 2023 </button>
+                    <button className="btn btn-light shadow-lg rounded-pill mx-2"> 2019 - 2023 </button>
+                    <button className="btn btn-light shadow-lg rounded-pill mx-2"> 2019 - 2023 </button>
+                    <button className="btn btn-light shadow-lg rounded-pill mx-2"> 2019 - 2023 </button>
+                </div>
+                <div className="add-sessions">
+                    <button className="btn btn-primary shadow-lg rounded-pill mx-2"> Add Session </button>
+                </div>
+            </div>
+
             <div className="row d-flex flwx-wrap justify-content-center ">
                 <div className="col-sm-6">
                     <div className="shadow-lg py-3 px-1 mx-1 rounded">
                         <h5 className="fw-bold m-2">Classroom</h5>
+                        
                     </div>
                 </div>
                 <div className="col-sm-6">
